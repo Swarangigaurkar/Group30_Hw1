53b36cd6a6f0dcac507477c168f35243f247f2cf
Leo Hsiang <yhsiang@ncsu.edu>
2022-08-30 08:44:08 -0400

    add comment to the test

--- test/test_sample.py
+++ test/test_sample.py
@@ -4,17 +4,19 @@ from Group_HW1.code.main import subtract
 from Group_HW1.code.main import multiply
 from Group_HW1.code.main import divide
 
+
+#test add method
 def test_add():
     assert add(3,4) == 7
 
-
+#test subtract method
 def test_sub():
     assert subtract(4,3) == 1
 
-
+#test multiply method
 def test_mul():
     assert multiply(3,4) == 12
 
-
+#test divide method
 def test_div():
     assert divide(3,1) == 3
